72e6af5de650057ce5f240b71ef9f4c2f4d23ec2
Naomi Mango <naomi.mango@gmail.com>
2021-11-15 11:29:47 +0100

    [NEW] Variables - Exercice 2

---
+++ Chapitre_2-Variables/exercice2.js
@@ -0,0 +1,13 @@
+/**
+ * LES VARIABLES
+ * —
+ * Exercice 2
+ */
+var score = 10;
+console.log("Le score est de " + score); // affichage du score => 10
+score = score + 10;
+console.log("Le score est de %d", score); // affichage du score avec un marqueur => 20
+score += 30;
+console.log('Le "score" est de ' + score); // affichage du score => 50
+score *= 3;
+console.log("Le \"score\" est de " + score); // affichage du score => 150
