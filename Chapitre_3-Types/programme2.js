cd9df99e79ef52bc317508caab458ced6924aad6
Naomi Mango <naomi.mango@gmail.com>
2021-11-15 11:53:53 +0100

    [MAJ] Types - Programme 2 v1

--- Chapitre_3-Types/programme2.js
+++ Chapitre_3-Types/programme2.js
@@ -9,3 +9,10 @@ var sexe = true;
 console.log("Le score est de " + score);
 console.log("Le nom est " + nom);
 console.log("Le sexe est " + sexe);
+
+var mot1 = "bonjour";
+var mot2 = "toto.";
+var mot3 = "Comment tu vas ?";
+
+var phrase = mot1 + " " + mot2 + " " + mot3;
+console.log(phrase);
