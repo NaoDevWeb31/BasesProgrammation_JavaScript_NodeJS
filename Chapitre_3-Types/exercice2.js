206c83f82b8d5b4873eec8ded999883c9c46a816
Naomi Mango <naomi.mango@gmail.com>
2021-11-15 12:41:10 +0100

    [NEW] Types - Exercice 2

---
+++ Chapitre_3-Types/exercice2.js
@@ -0,0 +1,11 @@
+/**
+ * LES TYPES
+ * —
+ * Exercice 2
+ */
+var note1 = 12
+var note2 = 14
+var note3 = 16;
+var moyenne = (note1 + note2 + note3) / 3;
+
+console.log("La moyenne de Marc est de %d", moyenne);
