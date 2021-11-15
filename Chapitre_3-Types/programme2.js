3c36317ed66d164b17ad9849a06a267f06e30ae2
Naomi Mango <naomi.mango@gmail.com>
2021-11-15 11:52:24 +0100

    [NEW] Types - Programme 2

---
+++ Chapitre_3-Types/programme2.js
@@ -0,0 +1,11 @@
+/**
+ * Mon Deuxième Programme
+ * utilisant les types
+ */
+
+var score = 10;
+var nom = "Milo";
+var sexe = true;
+console.log("Le score est de " + score);
+console.log("Le nom est " + nom);
+console.log("Le sexe est " + sexe);
