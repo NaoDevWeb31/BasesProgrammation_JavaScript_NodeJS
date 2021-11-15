6f6487968ddfeee43f92811537d94c904b757f0a
Naomi Mango <naomi.mango@gmail.com>
2021-11-15 11:18:42 +0100

    [MAJ] Variables - programme 1 v4

--- Chapitre_2-Variables/programme1.js
+++ Chapitre_2-Variables/programme1.js
@@ -1,6 +1,11 @@
+/**
+ * Mon Premier Programme
+ * utilisant des variables
+ */
+
 var score = 10;
-console.log("Le score est de : " + score);
-score = score + 10;
-console.log("Le score est de : " + score);
+console.log("Le score est de : " + score); // affichage du score => 10
+score = score + 10; // on ajoute à la variable score, son ancienne valeur plus 10
+console.log("Le score est de : " + score); // affichage du score => 20
 score += 10;
-console.log("Le score est de : " + score);
+console.log("Le score est de : " + score); // affichage du score => 30
