2506e4e71c818301cea08154aed7583d799c904d
Naomi Mango <naomi.mango@gmail.com>
2021-11-15 12:49:53 +0100

    [NEW] Types - Exercice 3

---
+++ Chapitre_3-Types/exercice3.js
@@ -0,0 +1,34 @@
+/**
+ * LES TYPES
+ * —
+ * Exercice 3
+ */
+var nom = "Tya";
+var age = 30;
+var sexe = true;
+var poids = 30;
+var yeux = "bleus";
+
+console.log(
+  "%s a %d ans, c'est une %s, elle pèse %d kilos et a les yeux %s",
+  nom,
+  age,
+  sexe,
+  poids,
+  yeux
+);
+
+nom = "Milo";
+age = 31;
+sexe = false;
+poids = 70;
+yeux = "marrons";
+
+console.log(
+  "%s a %d ans, c'est un %s, il pèse %d kilos et a les yeux %s",
+  nom,
+  age,
+  sexe,
+  poids,
+  yeux
+);
