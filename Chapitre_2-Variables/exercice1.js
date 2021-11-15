4b4a1b91e4c030678e6117c01ca834f573d9159e
Naomi Mango <naomi.mango@gmail.com>
2021-11-15 11:25:05 +0100

    [NEW] Variables - Exercice 1

---
+++ Chapitre_2-Variables/exercice1.js
@@ -0,0 +1,13 @@
+/**
+ * LES VARIABLES
+ * —
+ * Exercice 1
+ */
+var score = 10;
+console.log("Le score est de " + score);
+score = score + 10;
+console.log("Le score est de " + score);
+score = score + 10;
+score += 10;
+score += 10;
+console.log("Le \"score\" est de " + score);
