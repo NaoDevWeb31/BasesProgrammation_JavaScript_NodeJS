6007151ba0b9767a6ef32353d9854756fa870497
Naomi Mango <naomi.mango@gmail.com>
2021-11-16 12:27:17 +0100

    [NEW] Tests - Exercice 4

---
+++ Chapitre_4-Tests/exercice4.js
@@ -0,0 +1,15 @@
+/**
+ * LES TESTS
+ * —
+ * Exercice 4
+ */
+
+var force = 6;
+
+if (force < 3) {
+  console.log("Mage");
+} else if (force <= 5) {
+  console.log("Archer");
+} else {
+  console.log("Guerrier");
+}
