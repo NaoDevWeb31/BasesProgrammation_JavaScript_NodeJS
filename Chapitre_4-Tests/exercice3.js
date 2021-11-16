2a2e811348caab19b896e6c48f01dafff1411669
Naomi Mango <naomi.mango@gmail.com>
2021-11-16 12:20:48 +0100

    [MAJ] Tests - Exercice 3

--- Chapitre_4-Tests/exercice3.js
+++ Chapitre_4-Tests/exercice3.js
@@ -6,3 +6,10 @@
 
 var nom = "Matthieu";
 console.log(nom.substr(2, 2)); // string.substr(indice du caractère de départ, nombre de caractères à récupérer)
+
+nom = "Mathilde";
+var premiereLettre = nom.substr(0, 1);
+
+if (premiereLettre === "M") {
+  console.log("Bravo");
+}
