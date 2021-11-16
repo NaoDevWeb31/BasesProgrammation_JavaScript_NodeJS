48cda84c084dfd2312b17805688e64cd68008682
Naomi Mango <naomi.mango@gmail.com>
2021-11-16 12:17:24 +0100

    [NEW] Tests - Exercice 3

---
+++ Chapitre_4-Tests/exercice3.js
@@ -0,0 +1,8 @@
+/**
+ * LES TESTS
+ * —
+ * Exercice 3
+ */
+
+var nom = "Matthieu";
+console.log(nom.substr(2, 2)); // string.substr(indice du caractère de départ, nombre de caractères à récupérer)
