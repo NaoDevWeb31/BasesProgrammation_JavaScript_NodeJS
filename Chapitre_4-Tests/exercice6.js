5e6c8eff281baabf56bbb86bfbd5f3cb5558bd5e
Naomi Mango <naomi.mango@gmail.com>
2021-11-16 12:42:16 +0100

    [MAJ] Tests - Exercice 6

--- Chapitre_4-Tests/exercice6.js
+++ Chapitre_4-Tests/exercice6.js
@@ -7,29 +7,30 @@
 var readline = require("readline-sync");
 
 var moisEnLettre = readline.question("Saisir un mois de l'année (en lettte) : ");
+var txt = "Le mois de " + moisEnLettre + " correspond à ";
 
 if (moisEnLettre === "Janvier") {
-  console.log("Le mois de %s correspond à 1", moisEnLettre);
+  console.log(txt + "1");
 } else if (moisEnLettre === "Février") {
-  console.log("Le mois de %s correspond à 2", moisEnLettre);
+  console.log(txt + "2");
 } else if (moisEnLettre === "Mars") {
-  console.log("Le mois de %s correspond à 3", moisEnLettre);
+  console.log(txt + "3");
 } else if (moisEnLettre === "Avril") {
-  console.log("Le mois de %s correspond à 4", moisEnLettre);
+  console.log(txt + "4");
 } else if (moisEnLettre === "Mai") {
-  console.log("Le mois de %s correspond à 5", moisEnLettre);
+  console.log(txt + "5");
 } else if (moisEnLettre === "Juin") {
-  console.log("Le mois de %s correspond à 6", moisEnLettre);
+  console.log(txt + "6");
 } else if (moisEnLettre === "Juillet") {
-  console.log("Le mois de %s correspond à 7", moisEnLettre);
+  console.log(txt + "7");
 } else if (moisEnLettre === "Août") {
-  console.log("Le mois de %s correspond à 8", moisEnLettre);
+  console.log(txt + "8");
 } else if (moisEnLettre === "Septembre") {
-  console.log("Le mois de %s correspond à 9", moisEnLettre);
+  console.log(txt + "9");
 } else if (moisEnLettre === "Octobre") {
-  console.log("Le mois de %s correspond à 10", moisEnLettre);
+  console.log(txt + "10");
 } else if (moisEnLettre === "Novembre") {
-  console.log("Le mois de %s correspond à 11", moisEnLettre);
+  console.log(txt + "11");
 } else if (moisEnLettre === "Décembre") {
-  console.log("Le mois de %s correspond à 12", moisEnLettre);
+  console.log(txt + "12");
 }
