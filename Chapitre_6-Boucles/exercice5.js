3e1f40157858db5feca9816fd5544e3ffba1f627
Naomi Mango <naomi.mango@gmail.com>
2021-11-17 12:11:29 +0100

    [NEW] Boucles - Exercice 5

---
+++ Chapitre_6-Boucles/exercice5.js
@@ -0,0 +1,14 @@
+/**
+ * LES BOUCLES
+ * —
+ * Exercice 5
+ */
+
+for (let cpt1 = 1; cpt1 <= 5; cpt1++) {
+  var tirets = "";
+
+  for (var cpt = 1; cpt <= 20; cpt++) {
+    tirets += "-";
+  }
+  console.log(tirets);
+}
