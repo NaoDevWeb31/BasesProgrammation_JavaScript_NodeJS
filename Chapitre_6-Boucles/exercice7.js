8bd858877c42f078237027bb6837ae074af92be6
Naomi Mango <naomi.mango@gmail.com>
2021-11-17 12:27:52 +0100

    [NEW] Boucles - Exercice 7

---
+++ Chapitre_6-Boucles/exercice7.js
@@ -0,0 +1,14 @@
+/**
+ * LES BOUCLES
+ * —
+ * Exercice 7
+ */
+
+var chiffre = 4;
+var cpt = 10;
+
+while (cpt > 0) {
+  var resultat = cpt * chiffre;
+  console.log(cpt + " * " + chiffre + " = " + resultat);
+  cpt--;
+}
