34bcee568a1f8ebe9484bdedd010cde3afb37fa7
Naomi Mango <naomi.mango@gmail.com>
2021-11-17 11:51:25 +0100

    [NEW] Boucles - Exercice 3

---
+++ Chapitre_6-Boucles/exercice3.js
@@ -0,0 +1,15 @@
+/**
+ * LES BOUCLES
+ * —
+ * Exercice 3
+ */
+
+var resultat = 0;
+
+for (let cpt = 0; cpt <= 200; cpt = cpt + 2) {
+  resultat += cpt;
+}
+
+console.log(
+  "La somme des 100 premiers nombres pairs est égale à : " + resultat
+);
