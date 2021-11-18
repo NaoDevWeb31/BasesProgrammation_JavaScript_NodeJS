328daee961c400949faf5ef0cfe9eedd54e45459
Naomi Mango <naomi.mango@gmail.com>
2021-11-18 12:10:51 +0100

    [MAJ] Fonctions - Exercice 8

--- Chapitre_7-Fonctions/exercice8.js
+++ Chapitre_7-Fonctions/exercice8.js
@@ -10,20 +10,14 @@ var readline = require("readline-sync");
 var nbSaisi = readline.questionInt("Nombre à diviser : ");
 var diviseurSaisi = readline.questionInt("Le diviseur : ");
 
-var estDivisible = verifierParite(nbSaisi, diviseurSaisi);
-
-if (estDivisible) {
-    console.log("Le nombre " + nbSaisi + " est divisible par " + diviseurSaisi);
+if (verifierParite(nbSaisi, diviseurSaisi)) {
+  console.log("Le nombre " + nbSaisi + " est divisible par " + diviseurSaisi);
 } else {
-    var reste = nbSaisi % diviseurSaisi;
-    console.log("Le nombre " + nbSaisi + " n'est pas divisible par " + diviseurSaisi + " car le reste est de " + reste);
+  var reste = nbSaisi % diviseurSaisi;
+  console.log( "Le nombre " + nbSaisi + " n'est pas divisible par " + diviseurSaisi + " car le reste est de " + reste);
 }
 
 // Fonction
 function verifierParite(nombre, diviseur) {
-  if (nombre % diviseur === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return nombre % diviseur === 0;
 }
