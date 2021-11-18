260a7bd839081f04ed1a8156c3e170a1511ade77
Naomi Mango <naomi.mango@gmail.com>
2021-11-18 15:05:38 +0100

    [NEW] Tableaux - Exercice 1

---
+++ Chapitres_8-Tableaux/exercice1.js
@@ -0,0 +1,15 @@
+/**
+ * LES TABLEAUX
+ * -
+ * Exercice 1
+ */
+
+var noms = ["Matthieu", "Tya", "Milo"];
+console.log(noms); // [ 'Matthieu', 'Tya', 'Milo' ]
+
+console.log("Bonjour %s", noms[0]); // Bonjour Matthieu
+console.log("Hola %s", noms[1]); // Hola Tya
+console.log("Hello %s", noms[noms.length - 1]); // Hello Milo
+
+var pseudo = "bonjour";
+console.log(pseudo[3]); // j
