231b8d4a5e2f6e96755d8726c7dfc88a6c2548d3
Naomi Mango <naomi.mango@gmail.com>
2021-11-19 11:07:38 +0100

    [NEW] Tableaux Multi-Dimensionnels - Exercice 1

---
+++ Chapitre_9-TableauxMultiDimensionnels/exercice1.js
@@ -0,0 +1,15 @@
+/**
+ * LES TABLEAUX MULTI-DIMENSIONNELS
+ * -
+ * Exercice 1
+ */
+
+var tab = [
+  ["Tya", "MILO"],
+  ["Marco", "POLO"],
+  ["Pablo", "PICASSO"],
+];
+
+for (var i = 0; i < tab.length; i++) {
+  console.log(tab[i][0] + " - " + tab[i][1]);
+}
